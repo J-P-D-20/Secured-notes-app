408950bcc9ce60f5320b1ea4e1c8e51db97e91c3
J-P-D-20 <jhomary.dumanon@carsu.edu.ph>
2025-09-15 14:35:19 +0800

    update from main

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ import bcrypt from 'bcrypt';
 const app = express();
 app.use(express.json());
 
+const user = [
 
-app.get("/", (req, res) =>{
-    res.send("Hello world")
-    res.send("Tabibito")
-    res.send("Cabase")
-    res.send("Auditor")
-    res.send("Aguillas")
+]
+
+
+app.post('/registration' , async (req,res) => {
+    const {username,password,role} = req.body;
+    const hash = await bcrypt.hash(password, 13);
+
+    user.push({
+        username,
+        password: hash,
+        role
+    })
+
+    console.log(user);
+    res.send("Account Created Successfully")
 })
+
+
+
+
 
 
 
